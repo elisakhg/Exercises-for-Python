0f233a4bb9d8e156074bbbd63d645c0673a58194
elisakhg <151517501+elisakhg@users.noreply.github.com>
2023-11-29 12:31:26 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import mod
+#sum of two numbers
+mod.psum(10, 40)
+
+#product of two numbers
+mod.pmultiply(10, 40)
---
+++ mod.py
@@ -0,0 +1,7 @@
+
+
+def psum(number1, number2):
+    print("sum of numbers: ", number1+number2)
+
+def pmultiply(number1, number2):
+    print("sum of numbers: ", number1*number2)
