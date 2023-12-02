7161493b371c6689888403b3ef734edbc3c7feab
elisakhg <151517501+elisakhg@users.noreply.github.com>
2023-12-02 20:20:09 +0000

    Add files via upload

--- 3operators.py
+++ 3operators.py
@@ -4,8 +4,9 @@
 
 # 3.1.1 Multiply 4 with 8, and print the result.
 # TODO: Write your code below
-
+print(4*8)
 
 
 # 3.1.2 Divide 30 by 6, and print the result.
 # TODO: Write your code below
+print(30/6)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+#task 7.4.2
+
 import mod
 #sum of two numbers
 mod.psum(10, 40)
 
 #product of two numbers
 mod.pmultiply(10, 40)
+
+#task 7.4.3
+import platform
+
+x = dir(platform)
+print(x)
